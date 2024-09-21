bc12d7831eb74b3642ea39dc77516947cf2b2c9e
Ndegwa <ndegwas03@gmail.com>
2024-09-21 14:32:12 +0300

    all classes setup

--- model.py
+++ model.py
@@ -14,3 +14,29 @@ class Band(Base):
     hometown = Column(String)
 
     concerts = relationship('Concert', back_populates='band')
+
+#Venues
+class Venue(Base):
+    __tablename__ = 'venues'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    city = Column(String)
+
+    concerts = relationship('Concert', back_populates='venue')
+
+#Concerts
+class Concert(Base):
+    __tablename__ = 'concerts'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(String)
+    band_id = Column(Integer, ForeignKey('bands.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+
+    band = relationship('Band', back_populates='concerts')
+    venue = relationship('Venue', back_populates='concerts')
+
+# Database setup
+engine = create_engine('sqlite:///concerts.db')
+Base.metadata.create_all(engine)
